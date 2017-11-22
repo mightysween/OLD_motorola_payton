5ce865aef629c2cbb46fa469d1d0ecfe9155430b
mightysween <MTK@MTK.com>
2017-11-22 11:32:33 -0500

    build up vendor tree for TWRP decrypt

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,8 +27,6 @@ TW_CRYPTO_USE_SYSTEM_VOLD := true
 
 # Kernel
 TARGET_PREBUILT_KERNEL := device/motorola/payton/prebuilt
-#TARGET_KERNEL_CONFIG := /home/mightysween/omni/kernel/motorola/payton/arch/arm64
-#BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3 androidboot.hab.csv=1 androidboot.hab.product=payton androidboot.hab.cid=50 buildvariant=user veritykeyid=id:b640f6ee9102b88daa3450b13ef25fc9eb143d63
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
@@ -49,17 +47,20 @@ BOARD_SUPPRESS_SECURE_ERASE := true
 # TWRP
 RECOVERY_VARIANT := twrp
 TW_THEME := portrait_hdpi
+#TW_HAS_NO_RECOVERY_PARTITION := true
 #RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_EXCLUDE_SUPERSU := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 125
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TARGET_SYSTEM_PROP := device/motorola/payton/system.prop
 TARGET_RECOVERY_FSTAB := device/motorola/payton/twrp.fstab
 AB_OTA_UPDATER := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_INCLUDE_FB2PNG := true
 
 #TESTING
 
